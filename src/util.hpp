cb56feb78729c54772ee4ab574d1e058227529dd
Junta Mizuno <junta-m@ecei.tohoku.ac.jp>
2013-05-26 23:39:37 +0900

    Added util.hpp which contains some functions

---
+++ src/util.hpp
@@ -0,0 +1,22 @@
+#ifndef __UTIL_HPP__
+#define __UTIL_HPP__
+
+#include <string>
+#include <sstream>
+
+template< typename Tattr, typename Tspl >
+void join( std::string &str, std::vector< Tattr > vec, Tspl splitter ) {
+	std::stringstream ss;
+	if (vec.size() != 0) {
+		ss << vec[0];
+		if (vec.size() != 1) {
+			for (unsigned int i=1 ; i<vec.size() ; ++i) {
+				ss << splitter << vec[i];
+			}
+		}
+	}
+	str = ss.str();
+}
+
+#endif
+
