601138835df5b28eb2f35a71fa20ea6bc6a8a5af
Junta Mizuno <junta-m@ecei.tohoku.ac.jp>
2013-06-01 18:31:03 +0900

    Input single xml

--- src/oc2cab.cpp
+++ src/oc2cab.cpp
@@ -8,42 +8,55 @@
 #include "sentence.hpp"
 #include "modality.hpp"
 
-class modxml {
-	public:
-		std::string out_path;
-		boost::filesystem::path path;
-};
-
 int main(int argc, char *argv[]) {
+
+	boost::program_options::options_description opt("Usage");
+	opt.add_options()
+		("file,f", boost::program_options::value<std::string>(), "input xml file (required)")
+		("outdir,o", boost::program_options::value<std::string>(), "output directory (optional): default same as input xml file")
+		("help,h", "Show help messages")
+		("version,v", "Show version informaion");
+
+	boost::program_options::variables_map argmap;
+	boost::program_options::store(parse_command_line(argc, argv, opt), argmap);
+	boost::program_options::notify(argmap);
+
+	std::string xml;
+	if (argmap.count("file")) {
+		xml = argmap["file"].as<std::string>();
+	}
+	else {
+		std::cerr << "error: input xml file is required" << std::endl;
+		exit(-1);
+	}
+	std::cerr << "input: " << xml << std::endl;
+	boost::filesystem::path xml_path(xml);
+
+	std::string outdir;
+	if (argmap.count("outdir")) {
+		outdir = argmap["outdir"].as<std::string>();
+	}
+	else {
+		outdir = xml_path.parent_path().string();
+	}
+	std::cerr << "outdir: " << outdir << std::endl;
+	boost::filesystem::path outdir_path(outdir);
 
 	modality::parser mod_parser;
 
-	boost::filesystem::directory_iterator pos("/home/junta-m/work/20110324/TeaM/XML/OC");
-	boost::filesystem::directory_iterator last;
-	std::vector<boost::filesystem::path> xmls;
-	for (; pos!=last ; ++pos) {
-		boost::filesystem::path p(*pos);
-		xmls.push_back(p);
-	}
+	std::vector< std::vector< modality::t_token > > oc_sents = mod_parser.parse_OC(xml_path.string());
+	std::vector< nlp::sentence > parsed_sents;
+	int cnt = 0;
+	BOOST_FOREACH ( std::vector< modality::t_token > oc_sent, oc_sents ) {
+		nlp::sentence mod_ipa_sent = mod_parser.make_tagged_ipasents( oc_sent );
 
-	boost::filesystem::path out_dir("team");
-	std::vector<boost::filesystem::path>::iterator it_xml;
-	for (it_xml=xmls.begin() ; it_xml!=xmls.end() ; ++it_xml) {
-		std::cerr << it_xml->string()<< std::endl;
-
-		std::vector< std::vector< modality::t_token > > oc_sents = mod_parser.parse_OC(it_xml->string());
-		std::vector< nlp::sentence > parsed_sents;
-		int cnt = 0;
-		BOOST_FOREACH ( std::vector< modality::t_token > oc_sent, oc_sents ) {
-			nlp::sentence mod_ipa_sent = mod_parser.make_tagged_ipasents( oc_sent );
-
-			std::stringstream ss;
-			ss << (out_dir / it_xml->stem()).string() << "_" << std::setw(3) << std::setfill('0') << cnt << ".depmod";
-			std::ofstream os(ss.str().c_str());
-			os << mod_ipa_sent.cabocha() << std::endl;
-			os.close();
-			cnt++;
-		}
+		std::stringstream ss;
+		ss << (outdir_path / xml_path.stem()).string() << "_" << std::setw(3) << std::setfill('0') << cnt << ".depmod";
+		std::cerr << "output: " << ss.str() << std::endl;
+		std::ofstream os(ss.str().c_str());
+		os << mod_ipa_sent.cabocha() << std::endl;
+		os.close();
+		cnt++;
 	}
 
 	return true;
